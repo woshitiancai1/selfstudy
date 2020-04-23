88d2dd14d579a2da3971a06c007f0dc4982f2078
薛明星 <33643402+woshitiancai1@users.noreply.github.com>
2020-04-23 14:47:36 +0800

    Add files via upload

--- Vue.js/day-five/webpack-study/src/main.js
+++ Vue.js/day-five/webpack-study/src/main.js
@@ -1,16 +1,10 @@
-//项目JS的入口文件
-//为了解决浏览器中天生没有模块化的问题，ES6中提出了import和exports的概念
+//这是项目的入口文件
 
-//ES6
+//导入jquery
 import $ from 'jquery'
 
-//这是jquery的入口函数
+//实现奇偶行变色
 $(function(){
-    $('li:odd').css('backgrundColor','green');
-    $('li:even').css('backgrundColor',function(){
-        return '#'+'00ff00'
-    });
+    $('li:odd').css("backgroundColor","green");
+    $('li:even').css("backgroundColor","yellow");
 })
-
-//使用webpack-dev-server这个工具，能够提供类似于nodemon的功能，并自动编译项目代码
-//webpack-dev-server这个工具，要求在本地安装webpack
--- Vue.js/day-five/webpack-study/webpack.config.js
+++ Vue.js/day-five/webpack-study/webpack.config.js
@@ -1,15 +1,21 @@
-//默认情况下，直接使用webpack .\src\main.js -o  .\dist\bundle.js就能构建
-//如果只运行webpack命令的话，需要在项目的根目录中，而且，在这个配置文件中，
-//必须要显示要处理文件的路径和输出的文件的路径
+//使用Node语法，向外暴露配置对象，从而，让webpack运行的时候，加载指定的配置
+const path = require('path');
 
-//注意：这里使用Node中的语法向外暴露一个配置对象
-//因为webpack这个构建工具，底层就是使用Node开发出来的
+//1.导入在内存中生成页面的webpack插件
+const htmlWebpackPlugin = require("html-webpack-plugin")
 
-const path = require('path')
 module.exports = {
-    entry:path.join(__dirname,'./src/main.js'),//指定要处理的JS文件路径
-    output:{
-        path:path.join(__dirname,'./dist/'),//指定要输出文件的路径
-        filename:'bundle.js',//指定要输出的文件的名称
-    }
+    //配置npm run dev
+    entry:path.join(__dirname,'./src/main.js'),//项目的入口文件
+    output:{//打包好的文件的输出配置
+        path:path.join(__dirname,'/dist'),//输出文件路径
+        filename:'bundle.js'//输出文件名
+    },
+    //配置html-webpack插件
+    plugins:[
+        new htmlWebpackPlugin({//创建一个htmlWebpackPlugin的实例对象
+            template:path.join(__dirname,'./src/index.html'),//指定模板页面的路径
+            filename:'index.html'//指定内存中生成的html名称
+        })
+    ]
 }
