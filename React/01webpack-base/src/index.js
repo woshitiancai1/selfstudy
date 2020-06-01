7f642392ef5238eac1b8b5f869ac4edfd4778eb7
薛明星 <33643402+woshitiancai1@users.noreply.github.com>
2020-06-01 12:49:50 +0800

    Add files via upload

--- React/01webpack-base/src/index.js
+++ React/01webpack-base/src/index.js
@@ -2,7 +2,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const a = 10;
+//第一种创建组件的方式：
+function Hello(props) { //props接收到调用这个组件所传入的数据，是一个对象
+    // return null;表示此组件什么都不渲染
+    return <div > 这是一个合法的JSX虚拟DOM元素 { props.name } < /div>
+}
 
 //3.调用render函数
-ReactDOM.render( < div > < /div> , document.getElementById("app"))
+ReactDOM.render( < div >
+        <
+        Hello name = "zhangsan" / >
+        <
+        /div> , document.getElementById("app"))
